a9ea7fddc0351492df37d2a969ebe8faa8f74c92
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-12-19 21:32:58 -0800

    Even more organization, renaming and refactoring

--- src/value.rs
+++ src/value.rs
@@ -1,32 +1,7 @@
-use std::error::Error;
-use std::fmt::{Debug, Display, Formatter};
-use std::fmt;
+pub use self::boolean::Boolean;
+pub use self::null::Null;
+pub use self::string::String;
 
 mod null;
 mod boolean;
 mod string;
-
-pub type MarkerResult = Result<u8, ValueError>;
-
-pub trait Value {
-    fn get_marker(&self) -> MarkerResult;
-}
-
-#[derive(Debug)]
-pub struct ValueError {
-    message: String,
-}
-
-impl ValueError {
-    pub fn new(message: String) -> Self {
-        ValueError { message }
-    }
-}
-
-impl Display for ValueError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.write_str(&self.message)
-    }
-}
-
-impl Error for ValueError {}
