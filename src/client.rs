6a7dd34f44cf349922a64b2fb58c87262491067f
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-12-19 20:41:16 -0800

    Organize into modules, start writing value serialization logic

---
+++ src/client.rs
@@ -0,0 +1,31 @@
+use std::error::Error;
+use std::net::IpAddr;
+
+use bytes::*;
+use tokio::net::TcpStream;
+use tokio::prelude::*;
+
+const PREAMBLE: [u8; 4] = [0x60, 0x60, 0xB0, 0x17];
+const SUPPORTED_VERSIONS: [u32; 4] = [1, 0, 0, 0];
+
+pub struct Client {
+    stream: TcpStream
+}
+
+impl Client {
+    pub async fn new(host: IpAddr, port: usize) -> Result<Self, Box<dyn Error>> {
+        let client = Client {
+            stream: TcpStream::connect(format!("{}:{}", host, port)).await?
+        };
+        Ok(client)
+    }
+
+    pub async fn handshake(&mut self) -> Result<u32, Box<dyn Error>> {
+        let mut allowed_versions = BytesMut::with_capacity(16);
+        SUPPORTED_VERSIONS.iter().for_each(|&v| allowed_versions.put_u32(v));
+        self.stream.write(&mut PREAMBLE).await?;
+        self.stream.write_buf(&mut allowed_versions).await?;
+        self.stream.flush().await?;
+        Ok(self.stream.read_u32().await?)
+    }
+}
--- tests/bolt_client.rs
+++ tests/bolt_client.rs
@@ -1,7 +1,7 @@
-use rust_bolt::BoltClient;
+use rust_bolt::Client;
 
 #[tokio::test]
 async fn handshake() {
-    let mut client = BoltClient::new("127.0.0.1".parse().unwrap(), 7687).await.unwrap();
+    let mut client = Client::new("127.0.0.1".parse().unwrap(), 7687).await.unwrap();
     assert_eq!(client.handshake().await.unwrap(), 1);
 }
