911381e7d876f88cf8f95c8d29c2b55cd351846f
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-12-19 20:41:16 -0800

    Organize into modules, start writing value serialization logic

---
+++ src/messaging/value.rs
@@ -0,0 +1,28 @@
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
+use std::fmt;
+
+pub type MarkerResult = Result<u8, ValueError>;
+
+pub trait Value {
+    fn get_marker(&self) -> MarkerResult;
+}
+
+#[derive(Debug)]
+pub struct ValueError {
+    message: String,
+}
+
+impl ValueError {
+    pub fn new(message: String) -> Self {
+        ValueError { message }
+    }
+}
+
+impl Display for ValueError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.write_str(&self.message)
+    }
+}
+
+impl Error for ValueError {}
