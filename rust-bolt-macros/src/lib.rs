c9d0a612b313739e8f90ad06bf7371762350ef85
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-12-20 17:50:30 -0800

    Implement auto-derive macro for Serialize and Structure for message types
    - Be explicit that Map<K, V> generics implement TryInto<Bytes>

--- rust-bolt-macros/src/lib.rs
+++ rust-bolt-macros/src/lib.rs
@@ -2,11 +2,18 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 
-use quote::quote;
+use syn::{Data, DataStruct, Ident};
+
+use quote::{format_ident, quote};
 
 #[proc_macro_derive(Structure)]
 pub fn structure_derive(input: TokenStream) -> TokenStream {
     impl_structure(&syn::parse(input).unwrap())
+}
+
+#[proc_macro_derive(Serialize)]
+pub fn serialize_derive(input: TokenStream) -> TokenStream {
+    impl_serialize(&syn::parse(input).unwrap())
 }
 
 fn impl_structure(ast: &syn::DeriveInput) -> TokenStream {
@@ -29,4 +36,83 @@ fn impl_structure(ast: &syn::DeriveInput) -> TokenStream {
         }
     };
     gen.into()
+}
+
+const MARKER_TINY_STRUCTURE: u8 = 0xB0;
+const MARKER_SMALL_STRUCTURE: u8 = 0xDC;
+const MARKER_MEDIUM_STRUCTURE: u8 = 0xDD;
+
+fn impl_serialize(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let type_args = &ast.generics;
+    let where_clause = &ast.generics.where_clause;
+    let fields = match &ast.data {
+        Data::Struct(DataStruct {
+            struct_token: _,
+            fields,
+            semi_token: _,
+        }) => fields,
+        _ => panic!("Macro must be used on a struct."),
+    };
+    let marker = get_structure_marker(fields.len());
+
+    let byte_var_names: Vec<Ident> = fields
+        .iter()
+        .map(|f| format_ident!("{}_bytes", f.ident.clone().unwrap()))
+        .collect();
+
+    let byte_vars = byte_var_names.iter().zip(fields).map(|(var_name, field)| {
+        let field_name = field.ident.clone();
+        quote!(let #var_name = self.#field_name.try_into_bytes()?;)
+    });
+
+    let size_bytes = get_size_bytes(fields.len());
+
+    let gen = quote! {
+        impl#type_args crate::serialize::Serialize for #name#type_args
+        #where_clause
+        {
+            fn get_marker(&self) -> crate::serialize::SerializeResult<u8> {
+                Ok(#marker)
+            }
+        }
+
+        impl#type_args TryInto<::bytes::Bytes> for #name#type_args
+        #where_clause
+        {
+            type Error = crate::serialize::SerializeError;
+
+            fn try_into(self) -> crate::serialize::SerializeResult<::bytes::Bytes> {
+                let marker = self.get_marker()?;
+                let signature = self.get_signature();
+                #(#byte_vars)*
+                // Marker byte, up to 2 size bytes, signature byte, then the rest of the data
+                let mut result_bytes_mut = BytesMut::with_capacity(mem::size_of::<u8>() * 4 #(+ #byte_var_names.len())*);
+                result_bytes_mut.put_u8(marker);
+                #(result_bytes_mut.put_u8(#size_bytes);)*
+                result_bytes_mut.put_u8(signature);
+                #(result_bytes_mut.put(#byte_var_names);)*
+                Ok(result_bytes_mut.freeze())
+            }
+        }
+    };
+    gen.into()
+}
+
+fn get_structure_marker(num_fields: usize) -> u8 {
+    match num_fields {
+        0..=15 => MARKER_TINY_STRUCTURE | num_fields as u8,
+        16..=255 => MARKER_SMALL_STRUCTURE,
+        256..=65_535 => MARKER_MEDIUM_STRUCTURE,
+        _ => panic!("Too many fields in struct"),
+    }
+}
+
+fn get_size_bytes(num_fields: usize) -> Vec<u8> {
+    match num_fields {
+        0..=15 => vec![],
+        16..=255 => (num_fields as u8).to_be_bytes().to_vec(),
+        256..=65_535 => (num_fields as u16).to_be_bytes().to_vec(),
+        _ => panic!("Too many fields in struct"),
+    }
 }
