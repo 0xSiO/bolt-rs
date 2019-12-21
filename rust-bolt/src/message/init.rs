392e92846322479d823892a02a4e885e0e4a76ad
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-12-20 19:52:21 -0800

    Improve tests for Init

--- rust-bolt/src/message/init.rs
+++ rust-bolt/src/message/init.rs
@@ -29,11 +29,11 @@ mod tests {
 
     use crate::message::init::Init;
     use crate::serialize::Serialize;
+    use crate::structure::Structure;
     use crate::value::{Map, String};
 
-    #[test]
-    fn is_valid() {
-        let msg = Init {
+    fn new_msg() -> Init<String, String> {
+        Init {
             client_name: String {
                 value: "MyClient/1.0".to_string(),
             },
@@ -44,9 +44,23 @@ mod tests {
                         .map(|(k, v)| (String::from(k.to_string()), String::from(v.to_string()))),
                 ),
             },
-        };
+        }
+    }
+
+    #[test]
+    fn get_marker() {
+        assert_eq!(new_msg().get_marker().unwrap(), 0xB2);
+    }
+
+    #[test]
+    fn get_signature() {
+        assert_eq!(new_msg().get_signature(), 0x01);
+    }
+
+    #[test]
+    fn try_into_bytes() {
         assert_eq!(
-            msg.try_into_bytes().unwrap(),
+            new_msg().try_into_bytes().unwrap(),
             Bytes::from_static(&[
                 0xB2, 0x01, 0x8C, 0x4D, 0x79, 0x43, 0x6C, 0x69, 0x65, 0x6E, 0x74, 0x2F, 0x31, 0x2E,
                 0x30, 0xA1, 0x86, 0x73, 0x63, 0x68, 0x65, 0x6D, 0x65, 0x85, 0x62, 0x61, 0x73, 0x69,
