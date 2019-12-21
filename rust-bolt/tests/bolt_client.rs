fd8a9b82c7381d76bbab946c7d5d18c83921e1eb
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-12-20 20:08:52 -0800

    Add new_client helper to Client tests

--- rust-bolt/tests/bolt_client.rs
+++ rust-bolt/tests/bolt_client.rs
@@ -1,9 +1,13 @@
+use std::error::Error;
+
 use rust_bolt::Client;
+
+async fn new_client() -> Result<Client, Box<dyn Error>> {
+    Client::new("127.0.0.1".parse().unwrap(), 7687).await
+}
 
 #[tokio::test]
 async fn handshake() {
-    let mut client = Client::new("127.0.0.1".parse().unwrap(), 7687)
-        .await
-        .unwrap();
+    let mut client = new_client().await.unwrap();
     assert_eq!(client.handshake().await.unwrap(), 1);
 }
