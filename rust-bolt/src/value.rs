e9579c14e4cd3492cf6c3896310889c28dcc899f
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-12-21 21:07:01 -0800

    Start implementing general Value deserialization, hit a wall with TryFrom

--- rust-bolt/src/value.rs
+++ rust-bolt/src/value.rs
@@ -1,16 +1,19 @@
-use std::convert::TryInto;
+use std::convert::{TryFrom, TryInto};
 use std::hash::Hash;
 
-use bytes::Bytes;
+use bytes::{Buf, Bytes};
 use failure::Error;
 
-use crate::serialize::Serialize;
+use crate::serialize::{Deserialize, Serialize};
 
 pub use self::boolean::Boolean;
 pub use self::integer::Integer;
 pub use self::map::Map;
 pub use self::null::Null;
 pub use self::string::String;
+use crate::error::DeserializeError;
+use std::panic::catch_unwind;
+use std::sync::Mutex;
 
 mod boolean;
 mod integer;
@@ -56,5 +59,34 @@ impl TryInto<Bytes> for Value {
             Value::Null(null) => null.try_into(),
             Value::String(string) => string.try_into(),
         }
+    }
+}
+
+impl Deserialize<'_> for Value {}
+
+impl TryFrom<&mut Bytes> for Value {
+    type Error = Error;
+
+    fn try_from(input_bytes: &mut Bytes) -> Result<Self, Self::Error> {
+        let input_bytes = Mutex::new(input_bytes);
+        let result: Result<Value, Error> = catch_unwind(move || {
+            let input_bytes = input_bytes.lock().unwrap();
+            // TODO: Make sure clone() also preserves position of buffer cursor
+            let marker = input_bytes.clone().get_u8();
+
+            match marker {
+                // TODO: Can't do the below; try_from should take an Arc<Mutex<Bytes>>
+                // null::MARKER => Ok(Value::Null(Null::try_from(*input_bytes)?)),
+                // boolean::MARKER_FALSE | boolean::MARKER_TRUE => {
+                //     Ok(Value::Boolean(Boolean::try_from(*input_bytes)?))
+                // }
+                _ => todo!(),
+            }
+        })
+        .map_err(|_| DeserializeError("Panicked during deserialization".to_string()))?;
+
+        Ok(result.map_err(|err: Error| {
+            DeserializeError(format!("Error creating Value from Bytes: {}", err))
+        })?)
     }
 }
