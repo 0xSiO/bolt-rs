2141204da74e915c8a78ef351007c4ab5f73102f
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-12-23 12:24:58 -0800

    Implement last 3 value types: Relationship, Path, and UnboundRelationship

---
+++ rust-bolt/src/bolt/value/relationship.rs
@@ -0,0 +1,34 @@
+use std::convert::TryFrom;
+
+use failure::Error;
+
+use rust_bolt_macros::*;
+
+use crate::bolt::value::BoltValue;
+use crate::error::ValueError;
+
+pub const SIGNATURE: u8 = 0x52;
+
+#[derive(Debug, Clone, Hash, Eq, PartialEq, Signature, Marker, Serialize, Deserialize)]
+pub struct Relationship {
+    rel_identity: Box<BoltValue>,
+    start_node_identity: Box<BoltValue>,
+    end_node_identity: Box<BoltValue>,
+    rel_type: Box<BoltValue>,
+    properties: Box<BoltValue>,
+}
+
+// TODO: impl From<[Native Relationship type]> for Node
+
+impl TryFrom<BoltValue> for Relationship {
+    type Error = Error;
+
+    fn try_from(value: BoltValue) -> Result<Self, Self::Error> {
+        match value {
+            BoltValue::Relationship(rel) => Ok(rel),
+            _ => Err(ValueError::InvalidConversion(value).into()),
+        }
+    }
+}
+
+// TODO: impl From<[Native Relationship type]> for BoltValue
---
+++ rust-bolt/src/bolt/value/unbound_relationship.rs
@@ -0,0 +1,32 @@
+use std::convert::TryFrom;
+
+use failure::Error;
+
+use rust_bolt_macros::*;
+
+use crate::bolt::value::BoltValue;
+use crate::error::ValueError;
+
+pub const SIGNATURE: u8 = 0x72;
+
+#[derive(Debug, Clone, Hash, Eq, PartialEq, Signature, Marker, Serialize, Deserialize)]
+pub struct UnboundRelationship {
+    rel_identity: Box<BoltValue>,
+    rel_type: Box<BoltValue>,
+    properties: Box<BoltValue>,
+}
+
+// TODO: impl From<[Native UnboundRelationship type]> for Node
+
+impl TryFrom<BoltValue> for UnboundRelationship {
+    type Error = Error;
+
+    fn try_from(value: BoltValue) -> Result<Self, Self::Error> {
+        match value {
+            BoltValue::UnboundRelationship(unbound_rel) => Ok(unbound_rel),
+            _ => Err(ValueError::InvalidConversion(value).into()),
+        }
+    }
+}
+
+// TODO: impl From<[Native UnboundRelationship type]> for BoltValue
