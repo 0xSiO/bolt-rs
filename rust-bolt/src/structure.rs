d7b7719f87575768e1d1dffd141f88762a5b172c
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-12-22 22:13:25 -0800

    Automagically implement Deserialize in Structure derive macro

--- rust-bolt/src/structure.rs
+++ rust-bolt/src/structure.rs
@@ -1,11 +1,11 @@
 use crate::bolt::value::Marker;
-use crate::serialize::Serialize;
+use crate::serialize::{Deserialize, Serialize};
 
 pub const MARKER_TINY_STRUCTURE: u8 = 0xB0;
 pub const MARKER_SMALL_STRUCTURE: u8 = 0xDC;
 pub const MARKER_MEDIUM_STRUCTURE: u8 = 0xDD;
 
-pub trait Structure: Marker + Serialize {
+pub trait Structure: Marker + Serialize + Deserialize {
     fn get_signature(&self) -> u8;
 }
 
