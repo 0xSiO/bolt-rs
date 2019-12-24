bd6fd68f26120ba21a5cde69a92d31f7e99487e1
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-12-24 11:29:28 -0800

    Add conversions for native Relationship

--- bolt-proto/src/native/value/relationship.rs
+++ bolt-proto/src/native/value/relationship.rs
@@ -1,12 +1,42 @@
 use std::collections::HashMap;
+use std::convert::{TryFrom, TryInto};
 
-use crate::bolt::value::{Integer, Value};
+use failure::Error;
+
+use crate::bolt;
+use crate::bolt::value::Value;
+use crate::error::ValueError;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Relationship {
-    rel_identity: Integer,
-    start_node_identity: Integer,
-    end_node_identity: Integer,
-    rel_type: String,
-    properties: HashMap<String, Value>,
+    pub rel_identity: i64,
+    pub start_node_identity: i64,
+    pub end_node_identity: i64,
+    pub rel_type: String,
+    pub properties: HashMap<String, Value>,
+}
+
+impl TryFrom<bolt::value::Relationship> for Relationship {
+    type Error = Error;
+
+    fn try_from(bolt_rel: bolt::value::Relationship) -> Result<Self, Self::Error> {
+        Ok(Relationship {
+            rel_identity: i64::try_from(*bolt_rel.rel_identity)?,
+            start_node_identity: i64::try_from(*bolt_rel.start_node_identity)?,
+            end_node_identity: i64::try_from(*bolt_rel.end_node_identity)?,
+            rel_type: String::try_from(*bolt_rel.rel_type)?,
+            properties: (*bolt_rel.properties).try_into()?,
+        })
+    }
+}
+
+impl TryFrom<Value> for Relationship {
+    type Error = Error;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Relationship(rel) => Ok(Relationship::try_from(rel)?),
+            _ => Err(ValueError::InvalidConversion(value).into()),
+        }
+    }
 }
