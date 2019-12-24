ff527ab15012a101f0462e14ffd7ecc4510f4beb
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-12-24 12:04:42 -0800

    Add conversions for native UnboundRelationship

--- bolt-proto/src/native/value/unbound_relationship.rs
+++ bolt-proto/src/native/value/unbound_relationship.rs
@@ -1,10 +1,38 @@
 use std::collections::HashMap;
+use std::convert::{TryFrom, TryInto};
 
-use crate::bolt::value::{Integer, Value};
+use failure::Error;
+
+use crate::bolt;
+use crate::bolt::value::Value;
+use crate::error::ValueError;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct UnboundRelationship {
-    rel_identity: Integer,
-    rel_type: String,
-    properties: HashMap<String, Value>,
+    pub rel_identity: i64,
+    pub rel_type: String,
+    pub properties: HashMap<String, Value>,
+}
+
+impl TryFrom<bolt::value::UnboundRelationship> for UnboundRelationship {
+    type Error = Error;
+
+    fn try_from(bolt_ub_rel: bolt::value::UnboundRelationship) -> Result<Self, Self::Error> {
+        Ok(UnboundRelationship {
+            rel_identity: i64::try_from(*bolt_ub_rel.rel_identity)?,
+            rel_type: String::try_from(*bolt_ub_rel.rel_type)?,
+            properties: (*bolt_ub_rel.properties).try_into()?,
+        })
+    }
+}
+
+impl TryFrom<Value> for UnboundRelationship {
+    type Error = Error;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::UnboundRelationship(ub_rel) => Ok(UnboundRelationship::try_from(ub_rel)?),
+            _ => Err(ValueError::InvalidConversion(value).into()),
+        }
+    }
 }
