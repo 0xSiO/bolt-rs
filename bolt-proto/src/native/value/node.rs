366add994acb18d3001a6ff25e43e015e9b42aab
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-12-24 10:08:40 -0800

    Start to add conversions for native Node

--- bolt-proto/src/native/value/node.rs
+++ bolt-proto/src/native/value/node.rs
@@ -1,6 +1,11 @@
 use std::collections::HashMap;
+use std::convert::{TryFrom, TryInto};
 
+use failure::Error;
+
+use crate::bolt;
 use crate::bolt::value::Value;
+use crate::error::ValueError;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Node {
@@ -9,20 +14,18 @@ pub struct Node {
     pub properties: HashMap<String, Value>,
 }
 
-// TODO: Running into issues converting a Value to native type (Vec and HashMap)
-//       Make sure String implements TryFrom<Value>.
 //impl TryFrom<bolt::value::Node> for Node {
 //    type Error = Error;
 //
 //    fn try_from(bolt_node: bolt::value::Node) -> Result<Self, Self::Error> {
 //        Ok(Node {
 //            node_identity: i64::try_from(*bolt_node.node_identity)?,
-//            labels: Vec::try_from(*bolt_node.labels)?,
-//            properties: HashMap::try_from(*bolt_node.properties)?,
+//            labels: (*bolt_node.labels).try_into()?,
+//            properties: (*bolt_node.properties).try_into()?,
 //        })
 //    }
 //}
-
+//
 //impl TryFrom<Value> for Node {
 //    type Error = Error;
 //
