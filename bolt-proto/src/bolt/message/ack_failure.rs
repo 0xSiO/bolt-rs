6c73df89b8a434ce21da367fa0bcbc4d74bf3dcb
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-12-24 21:02:07 -0800

    Remove Message variant data and their modules for empty serialized structures

--- bolt-proto/src/bolt/message/ack_failure.rs
+++ bolt-proto/src/bolt/message/ack_failure.rs
@@ -1,34 +1,9 @@
-use std::convert::TryFrom;
-
-use failure::Error;
-
 use bolt_proto_derive::*;
-
-use crate::bolt::Message;
-use crate::error::MessageError;
-use crate::native;
 
 pub(crate) const SIGNATURE: u8 = 0x0E;
 
 #[derive(Debug, Signature, Marker, Serialize, Deserialize)]
 pub struct AckFailure;
-
-impl From<native::message::AckFailure> for AckFailure {
-    fn from(_native_ack_failure: native::message::AckFailure) -> Self {
-        Self
-    }
-}
-
-impl TryFrom<Message> for AckFailure {
-    type Error = Error;
-
-    fn try_from(message: Message) -> Result<Self, Self::Error> {
-        match message {
-            Message::AckFailure(ack_failure) => Ok(ack_failure),
-            _ => Err(MessageError::InvalidConversion(message).into()),
-        }
-    }
-}
 
 #[cfg(test)]
 mod tests {
--- bolt-proto/src/bolt/message/pull_all.rs
+++ bolt-proto/src/bolt/message/pull_all.rs
@@ -1,34 +1,9 @@
-use std::convert::TryFrom;
-
-use failure::Error;
-
 use bolt_proto_derive::*;
-
-use crate::bolt::Message;
-use crate::error::MessageError;
-use crate::native;
 
 pub(crate) const SIGNATURE: u8 = 0x3F;
 
 #[derive(Debug, Signature, Marker, Serialize, Deserialize)]
 pub struct PullAll;
-
-impl From<native::message::PullAll> for PullAll {
-    fn from(_native_pull_all: native::message::PullAll) -> Self {
-        Self
-    }
-}
-
-impl TryFrom<Message> for PullAll {
-    type Error = Error;
-
-    fn try_from(message: Message) -> Result<Self, Self::Error> {
-        match message {
-            Message::PullAll(pull_all) => Ok(pull_all),
-            _ => Err(MessageError::InvalidConversion(message).into()),
-        }
-    }
-}
 
 #[cfg(test)]
 mod tests {
