805cc176854a1c32e1a55dabfabe57f9b6ea9771
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-01-29 11:21:09 -0800

    Support secure TCP streams for bb8 connection pool
    - Use futures::future::join_all in basic_pool test instead of waiting some amount of time for tasks to finish

--- bb8-bolt/src/lib.rs
+++ bb8-bolt/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::convert::TryFrom;
-use std::net::IpAddr;
+use std::net::{SocketAddr, ToSocketAddrs};
 
 use bb8::ManageConnection;
 use failure::{Error, Fail};
@@ -10,30 +10,35 @@ use bolt_client::message::Success;
 use bolt_client::{Client, Message};
 
 pub struct BoltConnectionManager {
-    host: IpAddr,
-    port: u16,
+    addr: SocketAddr,
+    domain: Option<String>,
     client_name: String,
     auth_token: HashMap<String, String>,
 }
 
 impl BoltConnectionManager {
     pub fn new(
-        host: IpAddr,
-        port: u16,
+        addr: impl ToSocketAddrs,
+        domain: Option<String>,
         client_name: String,
         auth_token: HashMap<String, String>,
-    ) -> Self {
-        Self {
-            host,
-            port,
+    ) -> Result<Self, Error> {
+        Ok(Self {
+            addr: addr
+                .to_socket_addrs()?
+                .next()
+                .ok_or_else(|| BoltConnectionError::InvalidAddress)?,
+            domain,
             client_name,
             auth_token,
-        }
+        })
     }
 }
 
 #[derive(Debug, Fail)]
 pub enum BoltConnectionError {
+    #[fail(display = "Failed to connect: invalid host address.")]
+    InvalidAddress,
     #[fail(display = "Initialization of client failed: received {:?}", _0)]
     ClientInitFailed(Message),
 }
@@ -44,8 +49,11 @@ impl ManageConnection for BoltConnectionManager {
     type Error = Error;
 
     async fn connect(&self) -> Result<Self::Connection, Self::Error> {
-        // TODO: Support secure TCP client
-        let mut client = Client::new_tcp((self.host, self.port)).await?;
+        let mut client = match &self.domain {
+            Some(domain) => Client::new_secure_tcp(domain, self.addr).await?,
+            None => Client::new_tcp(self.addr).await?,
+        };
+
         let response = client
             .init(self.client_name.clone(), self.auth_token.clone())
             .await?;
@@ -73,54 +81,87 @@ impl ManageConnection for BoltConnectionManager {
 
 #[cfg(test)]
 mod tests {
+    use std::env;
     use std::iter::FromIterator;
-    use std::time::Duration;
 
     use bb8::*;
 
     use super::*;
 
-    #[tokio::test]
-    async fn basic_pool() {
-        let manager = BoltConnectionManager::new(
-            "127.0.0.1".parse().unwrap(),
-            7687,
+    fn get_connection_manager() -> BoltConnectionManager {
+        let (addr, domain, username, password) = match env::var("BOLT_CLIENT_TEST_REMOTE") {
+            Ok(domain) => {
+                let login_info = env::var("BOLT_CLIENT_TEST_LOGIN").unwrap();
+                let login_info: Vec<&str> = login_info.split(",").collect();
+                (
+                    domain.clone() + ":" + &env::var("BOLT_CLIENT_TEST_PORT").unwrap(),
+                    Some(domain),
+                    login_info[0].to_string(),
+                    login_info[1].to_string(),
+                )
+            }
+            Err(_) => (
+                "127.0.0.1:7687".to_string(),
+                None,
+                "neo4j".to_string(),
+                "test".to_string(),
+            ),
+        };
+
+        BoltConnectionManager::new(
+            addr,
+            domain,
             "bolt-client/X.Y.Z".to_string(),
             HashMap::from_iter(vec![
                 (String::from("scheme"), String::from("basic")),
-                (String::from("principal"), String::from("neo4j")),
-                (String::from("credentials"), String::from("test")),
+                (String::from("principal"), username),
+                (String::from("credentials"), password),
             ]),
-        );
+        )
+        .unwrap()
+    }
+
+    #[tokio::test]
+    async fn basic_pool() {
+        let manager = get_connection_manager();
         let pool = Pool::builder().max_size(15).build(manager).await.unwrap();
 
-        for i in 1..=30 {
+        let mut tasks = Vec::with_capacity(50);
+        for i in 1..=tasks.capacity() {
             let pool = pool.clone();
-            tokio::spawn(async move {
+            tasks.push(tokio::spawn(async move {
                 let mut conn = pool.get().await.unwrap();
                 let statement = format!("RETURN {} as num;", i);
                 conn.run(statement, None).await.unwrap();
                 let (response, records) = conn.pull_all().await.unwrap();
                 assert!(Success::try_from(response).is_ok());
-                assert_eq!(i32::try_from(records[0].fields()[0].clone()).unwrap(), i);
-            });
+                assert_eq!(
+                    i32::try_from(records[0].fields()[0].clone()).unwrap(),
+                    i as i32
+                );
+            }));
         }
-        tokio::time::delay_for(Duration::from_millis(50)).await;
+        tokio::join!(futures::future::join_all(tasks));
     }
 
     #[tokio::test]
     async fn invalid_init_fails() {
-        let manager = BoltConnectionManager::new(
-            "127.0.0.1".parse().unwrap(),
-            7687,
+        let invalid_manager = BoltConnectionManager::new(
+            "127.0.0.1:7687",
+            None,
             "bolt-client/X.Y.Z".to_string(),
             HashMap::from_iter(vec![
                 (String::from("scheme"), String::from("basic")),
                 (String::from("principal"), String::from("neo4j")),
                 (String::from("credentials"), String::from("invalid")),
             ]),
-        );
-        let pool = Pool::builder().max_size(2).build(manager).await.unwrap();
+        )
+        .unwrap();
+        let pool = Pool::builder()
+            .max_size(2)
+            .build(invalid_manager)
+            .await
+            .unwrap();
         let conn = pool.dedicated_connection().await;
         assert!(conn.is_err());
     }
