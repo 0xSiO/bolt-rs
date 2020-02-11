9918fee89fcde4d34ce2f5b7844ff4b539bcdadc
Luc Street <6709262+0xSiO@users.noreply.github.com>
2020-02-10 16:34:54 -0800

    Use tokio-rustls for TLS-secured Clients
    - Go back to only supporting Bolt v1 clients, still some work that needs to be done before we can support v2+

--- bolt-client/src/client.rs
+++ bolt-client/src/client.rs
@@ -1,9 +1,14 @@
 use std::convert::TryInto;
+use std::sync::Arc;
 
 use bytes::*;
 use tokio::io::BufStream;
 use tokio::net::{TcpStream, ToSocketAddrs};
 use tokio::prelude::*;
+use tokio_rustls::rustls::ClientConfig;
+use tokio_rustls::webpki::DNSNameRef;
+use tokio_rustls::{webpki, TlsConnector};
+use webpki_roots::TLS_SERVER_ROOTS;
 
 use bolt_proto::Message;
 
@@ -14,7 +19,7 @@ mod v1;
 mod v3;
 
 const PREAMBLE: [u8; 4] = [0x60, 0x60, 0xB0, 0x17];
-const SUPPORTED_VERSIONS: [u32; 4] = [4, 3, 2, 1];
+const SUPPORTED_VERSIONS: [u32; 4] = [1, 0, 0, 0];
 
 #[derive(Debug)]
 pub struct Client {
@@ -27,9 +32,12 @@ impl Client {
     /// attempt to connect to the server over a connection secured with TLS.
     pub async fn new(addr: impl ToSocketAddrs, domain: Option<&str>) -> Result<Self> {
         let stream = match domain {
-            Some(domain) => Stream::SecureTcp(
-                async_native_tls::connect(&domain, TcpStream::connect(addr).await?).await?,
-            ),
+            Some(domain) => {
+                let tls_connector = Client::configure_tls_connector(&TLS_SERVER_ROOTS);
+                let dns_name_ref = DNSNameRef::try_from_ascii_str(&domain)?;
+                let stream = TcpStream::connect(addr).await?;
+                Stream::SecureTcp(tls_connector.connect(dns_name_ref, stream).await?)
+            }
             None => Stream::Tcp(TcpStream::connect(addr).await?),
         };
         let mut client = Client {
@@ -38,6 +46,12 @@ impl Client {
         };
         client.version = client.handshake().await?;
         Ok(client)
+    }
+
+    fn configure_tls_connector(root_certs: &webpki::TLSServerTrustAnchors) -> TlsConnector {
+        let mut config = ClientConfig::new();
+        config.root_store.add_server_trust_anchors(root_certs);
+        TlsConnector::from(Arc::new(config))
     }
 
     async fn handshake(&mut self) -> Result<u32> {
