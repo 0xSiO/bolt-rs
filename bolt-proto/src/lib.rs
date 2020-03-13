5db5cf7e2d45b83b57d77b860865d128cdd006e2
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-03-13 15:19:42 -0700

    Implement DateTimeOffset value type
    - Add neat macro to impl TryFrom<Value>

--- bolt-proto/src/lib.rs
+++ bolt-proto/src/lib.rs
@@ -6,3 +6,20 @@ pub mod error;
 pub mod message;
 mod serialization;
 pub mod value;
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! impl_try_from_value {
+    ($T:ident) => {
+        impl ::std::convert::TryFrom<$crate::Value> for $T {
+            type Error = $crate::error::Error;
+
+            fn try_from(value: $crate::Value) -> $crate::error::Result<Self> {
+                match value {
+                    $crate::Value::$T(inner) => Ok(inner),
+                    _ => Err($crate::error::Error::InvalidValueConversion(value).into()),
+                }
+            }
+        }
+    };
+}
