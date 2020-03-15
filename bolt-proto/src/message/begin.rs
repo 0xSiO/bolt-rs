34a51a1e3ad3ce81cfbc5cc9ebdfeb01572b550e
Luc Street <6709262+0xSiO@users.noreply.github.com>
2020-03-15 16:37:39 -0700

    Implement Begin message

---
+++ bolt-proto/src/message/begin.rs
@@ -0,0 +1,120 @@
+use std::collections::HashMap;
+use std::convert::TryFrom;
+
+use bolt_proto_derive::*;
+
+use crate::error::*;
+use crate::{Message, Value};
+
+pub(crate) const MARKER: u8 = 0xB1;
+pub(crate) const SIGNATURE: u8 = 0x11;
+
+#[derive(Debug, Clone, Eq, PartialEq, Signature, Marker, Serialize, Deserialize)]
+pub struct Begin {
+    pub(crate) metadata: HashMap<String, Value>,
+}
+
+impl Begin {
+    pub fn new(metadata: HashMap<String, Value>) -> Self {
+        Self { metadata }
+    }
+
+    pub fn metadata(&self) -> &HashMap<String, Value> {
+        &self.metadata
+    }
+}
+
+impl TryFrom<Message> for Begin {
+    type Error = Error;
+
+    fn try_from(message: Message) -> Result<Self> {
+        match message {
+            Message::Begin(begin) => Ok(begin),
+            _ => Err(ConversionError::FromMessage(message).into()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use std::iter::FromIterator;
+    use std::sync::{Arc, Mutex};
+
+    use bytes::Bytes;
+
+    use crate::serialization::*;
+    use crate::value::*;
+
+    use super::*;
+
+    fn new_msg() -> Begin {
+        Begin::new(HashMap::from_iter(vec![(
+            "arbitrary".to_string(),
+            Value::from("meh"),
+        )]))
+    }
+
+    #[test]
+    fn get_marker() {
+        assert_eq!(new_msg().get_marker().unwrap(), MARKER);
+    }
+
+    #[test]
+    fn get_signature() {
+        assert_eq!(new_msg().get_signature(), SIGNATURE);
+    }
+
+    #[test]
+    fn try_into_bytes() {
+        let msg = new_msg();
+        assert_eq!(
+            msg.try_into_bytes().unwrap(),
+            Bytes::from_static(&[
+                MARKER,
+                SIGNATURE,
+                map::MARKER_TINY | 1,
+                string::MARKER_TINY | 9,
+                b'a',
+                b'r',
+                b'b',
+                b'i',
+                b't',
+                b'r',
+                b'a',
+                b'r',
+                b'y',
+                string::MARKER_TINY | 3,
+                b'm',
+                b'e',
+                b'h',
+            ])
+        );
+    }
+
+    #[test]
+    fn try_from_bytes() {
+        let msg = new_msg();
+        let msg_bytes = &[
+            map::MARKER_TINY | 1,
+            string::MARKER_TINY | 9,
+            b'a',
+            b'r',
+            b'b',
+            b'i',
+            b't',
+            b'r',
+            b'a',
+            b'r',
+            b'y',
+            string::MARKER_TINY | 3,
+            b'm',
+            b'e',
+            b'h',
+        ];
+        assert_eq!(
+            Begin::try_from(Arc::new(Mutex::new(Bytes::from_static(msg_bytes)))).unwrap(),
+            msg
+        );
+    }
+}
