bbc2c9080d65d0479ab5e83e9ba85b1e33d9dc19
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-03-15 19:28:24 -0700

    Improve tests for Record message type

--- bolt-proto/src/message/record.rs
+++ bolt-proto/src/message/record.rs
@@ -24,16 +24,107 @@ impl_try_from_message!(Record, Record);
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use std::convert::TryFrom;
+    use std::iter::FromIterator;
     use std::sync::{Arc, Mutex};
 
     use bytes::Bytes;
 
+    use crate::serialization::*;
+    use crate::value::*;
+
     use super::*;
+
+    fn new_msg() -> Record {
+        Record::new(vec![
+            Value::from(1200_i16),
+            Value::from("hi there"),
+            Value::from(HashMap::from_iter(vec![(
+                "key".to_string(),
+                Value::from("value"),
+            )])),
+        ])
+    }
+
+    #[test]
+    fn get_marker() {
+        assert_eq!(new_msg().get_marker().unwrap(), MARKER);
+    }
+
+    #[test]
+    fn get_signature() {
+        assert_eq!(new_msg().get_signature(), SIGNATURE);
+    }
+
+    #[test]
+    fn try_into_bytes() {
+        let msg = new_msg();
+        assert_eq!(
+            msg.try_into_bytes().unwrap(),
+            Bytes::from_static(&[
+                MARKER,
+                SIGNATURE,
+                list::MARKER_TINY | 3,
+                integer::MARKER_INT_16,
+                0x04,
+                0xB0,
+                string::MARKER_TINY | 8,
+                b'h',
+                b'i',
+                b' ',
+                b't',
+                b'h',
+                b'e',
+                b'r',
+                b'e',
+                map::MARKER_TINY | 1,
+                string::MARKER_TINY | 3,
+                b'k',
+                b'e',
+                b'y',
+                string::MARKER_TINY | 5,
+                b'v',
+                b'a',
+                b'l',
+                b'u',
+                b'e'
+            ])
+        );
+    }
 
     #[test]
     fn try_from_bytes() {
-        let bytes = Bytes::from_static(&[0x93, 0x01, 0x02, 0x03]);
-        assert!(Record::try_from(Arc::new(Mutex::new(bytes))).is_ok());
+        let msg = new_msg();
+        let msg_bytes = &[
+            list::MARKER_TINY | 3,
+            integer::MARKER_INT_16,
+            0x04,
+            0xB0,
+            string::MARKER_TINY | 8,
+            b'h',
+            b'i',
+            b' ',
+            b't',
+            b'h',
+            b'e',
+            b'r',
+            b'e',
+            map::MARKER_TINY | 1,
+            string::MARKER_TINY | 3,
+            b'k',
+            b'e',
+            b'y',
+            string::MARKER_TINY | 5,
+            b'v',
+            b'a',
+            b'l',
+            b'u',
+            b'e',
+        ];
+        assert_eq!(
+            Record::try_from(Arc::new(Mutex::new(Bytes::from_static(msg_bytes)))).unwrap(),
+            msg
+        );
     }
 }
