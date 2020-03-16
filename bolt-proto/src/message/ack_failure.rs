2e57db31088ae75ae3d096b91fe8a56ec124eadd
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-03-15 19:08:52 -0700

    Use new test-generating macro for empty message types

--- bolt-proto/src/message/ack_failure.rs
+++ bolt-proto/src/message/ack_failure.rs
@@ -7,41 +7,4 @@ pub(crate) const SIGNATURE: u8 = 0x0E;
 pub struct AckFailure;
 
 #[cfg(test)]
-mod tests {
-    use bytes::Bytes;
-    use std::convert::TryFrom;
-    use std::sync::{Arc, Mutex};
-
-    use crate::serialization::*;
-
-    use super::*;
-
-    #[test]
-    fn get_marker() {
-        assert_eq!(AckFailure.get_marker().unwrap(), MARKER);
-    }
-
-    #[test]
-    fn get_signature() {
-        assert_eq!(AckFailure.get_signature(), SIGNATURE);
-    }
-
-    #[test]
-    fn try_into_bytes() {
-        let msg = AckFailure;
-        assert_eq!(
-            msg.try_into_bytes().unwrap(),
-            Bytes::from_static(&[MARKER, SIGNATURE])
-        );
-    }
-
-    #[test]
-    fn try_from_bytes() {
-        let msg = AckFailure;
-        let msg_bytes = &[];
-        assert_eq!(
-            AckFailure::try_from(Arc::new(Mutex::new(Bytes::from_static(msg_bytes)))).unwrap(),
-            msg
-        );
-    }
-}
+crate::impl_empty_message_tests!(AckFailure);
--- bolt-proto/src/message/ignored.rs
+++ bolt-proto/src/message/ignored.rs
@@ -7,19 +7,4 @@ pub(crate) const SIGNATURE: u8 = 0x7E;
 pub struct Ignored;
 
 #[cfg(test)]
-mod tests {
-    use std::convert::TryFrom;
-    use std::sync::{Arc, Mutex};
-
-    use bytes::Bytes;
-
-    use super::*;
-
-    #[test]
-    fn try_from_bytes() {
-        // No data needed!
-        let bytes = Bytes::from_static(&[]);
-        let ignored = Ignored::try_from(Arc::new(Mutex::new(bytes)));
-        assert!(ignored.is_ok());
-    }
-}
+crate::impl_empty_message_tests!(Ignored);
