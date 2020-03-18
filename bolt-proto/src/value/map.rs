69695bd70c536156887aae20761e71ba9fa5f8dd
Luc Street <6709262+0xSiO@users.noreply.github.com>
2020-03-18 15:36:43 -0700

    Correctly calculate number of bytes needed to serialize a Map

--- bolt-proto/src/value/map.rs
+++ bolt-proto/src/value/map.rs
@@ -68,19 +68,31 @@ impl TryInto<Bytes> for Map {
 
     fn try_into(self) -> Result<Bytes> {
         let marker = self.get_marker()?;
-        // TODO: Calculate length correctly
-        let mut bytes = BytesMut::with_capacity(mem::size_of::<Value>() * 2 * self.value.len());
-        bytes.put_u8(marker);
-        match self.value.len() {
-            0..=15 => {}
-            16..=255 => bytes.put_u8(self.value.len() as u8),
-            256..=65_535 => bytes.put_u16(self.value.len() as u16),
-            65_536..=4_294_967_295 => bytes.put_u32(self.value.len() as u32),
-            _ => return Err(Error::ValueTooLarge(self.value.len())),
+        let length = self.value.len();
+
+        let mut total_value_bytes: usize = 0;
+        let mut value_bytes_vec: Vec<Bytes> = Vec::with_capacity(length);
+        for (key, val) in self.value {
+            let key_bytes: Bytes = key.try_into()?;
+            let val_bytes: Bytes = val.try_into()?;
+            total_value_bytes += key_bytes.len() + val_bytes.len();
+            value_bytes_vec.push(key_bytes);
+            value_bytes_vec.push(val_bytes);
         }
-        for (key, value) in self.value {
-            bytes.put(&mut key.try_into_bytes().unwrap());
-            bytes.put(&mut value.try_into_bytes().unwrap());
+        // Worst case is a large Map, with marker byte, 32-bit size value, and all the Value bytes
+        let mut bytes = BytesMut::with_capacity(
+            mem::size_of::<u8>() + mem::size_of::<u32>() + total_value_bytes,
+        );
+        bytes.put_u8(marker);
+        match length {
+            0..=15 => {}
+            16..=255 => bytes.put_u8(length as u8),
+            256..=65_535 => bytes.put_u16(length as u16),
+            65_536..=4_294_967_295 => bytes.put_u32(length as u32),
+            _ => return Err(Error::ValueTooLarge(length)),
+        }
+        for value_bytes in value_bytes_vec {
+            bytes.put(value_bytes);
         }
         Ok(bytes.freeze())
     }
