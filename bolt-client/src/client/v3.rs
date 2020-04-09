bd2a4890f10d6dce7a7574c6f5cfd4d66f44d3fa
Luc Street <6709262+0xSiO@users.noreply.github.com>
2020-04-09 14:49:14 -0700

    Test begin, commit, and rollback for v3 clients

--- bolt-client/src/client/v3.rs
+++ bolt-client/src/client/v3.rs
@@ -40,24 +40,22 @@ impl Client {
     }
 
     #[bolt_version(3, 4)]
+    // TODO: The impl Into<Value> is nice, but makes empty maps tricky. Maybe wrap the HashMap in a Metadata type
     pub async fn begin(&mut self, metadata: HashMap<String, impl Into<Value>>) -> Result<Message> {
         let begin_msg = Begin::new(metadata.into_iter().map(|(k, v)| (k, v.into())).collect());
         self.send_message(Message::Begin(begin_msg)).await?;
-        // TODO: Is there actually a response?
         self.read_message().await
     }
 
     #[bolt_version(3, 4)]
     pub async fn commit(&mut self) -> Result<Message> {
         self.send_message(Message::Commit).await?;
-        // TODO: Is there actually a response?
         self.read_message().await
     }
 
     #[bolt_version(3, 4)]
     pub async fn rollback(&mut self) -> Result<Message> {
         self.send_message(Message::Rollback).await?;
-        // TODO: Is there actually a response?
         self.read_message().await
     }
 }
@@ -115,11 +113,11 @@ mod tests {
         let mut client = client.unwrap();
         let messages = vec![
             Message::RunWithMetadata(RunWithMetadata::new(
-                "MATCH (n {test: 'v3-pipelined'}) DETACH DELETE n;".to_string(), 
+                "MATCH (n {test: 'v3-pipelined'}) DETACH DELETE n;".to_string(),
                 Default::default(), Default::default())),
             Message::PullAll,
             Message::RunWithMetadata(RunWithMetadata::new(
-                "CREATE (:Database {name: 'neo4j', v1_release: date('2010-02-16'), test: 'v3-pipelined'});".to_string(), 
+                "CREATE (:Database {name: 'neo4j', v1_release: date('2010-02-16'), test: 'v3-pipelined'});".to_string(),
                 Default::default(), Default::default())),
             Message::PullAll,
             Message::RunWithMetadata(RunWithMetadata::new(
@@ -144,5 +142,136 @@ mod tests {
                 _ => false,
             });
         }
+    }
+
+    #[tokio::test]
+    async fn begin() {
+        let client = get_initialized_client(3).await;
+        skip_if_handshake_failed!(client);
+        let mut client = client.unwrap();
+        let metadata = HashMap::<std::string::String, bool>::new(); // dummy empty metadata
+        let response = client.begin(metadata).await.unwrap();
+        assert!(Success::try_from(response).is_ok());
+    }
+
+    #[tokio::test]
+    async fn commit_empty_transaction() {
+        let client = get_initialized_client(3).await;
+        skip_if_handshake_failed!(client);
+        let mut client = client.unwrap();
+        let metadata = HashMap::<std::string::String, bool>::new(); // dummy empty metadata
+        client.begin(metadata).await.unwrap();
+        let response = client.commit().await.unwrap();
+        assert!(Success::try_from(response).is_ok());
+    }
+
+    #[tokio::test]
+    async fn commit() {
+        let client = get_initialized_client(3).await;
+        skip_if_handshake_failed!(client);
+        let mut client = client.unwrap();
+        let metadata = HashMap::<std::string::String, bool>::new(); // dummy empty metadata
+        client.begin(metadata).await.unwrap();
+
+        let messages = vec![
+            Message::RunWithMetadata(RunWithMetadata::new(
+                "MATCH (n {test: 'v3-commit'}) DETACH DELETE n;".to_string(),
+                Default::default(), Default::default())),
+            Message::PullAll,
+            Message::RunWithMetadata(RunWithMetadata::new(
+                "CREATE (:Database {name: 'neo4j', v1_release: date('2010-02-16'), test: 'v3-commit'});".to_string(),
+                Default::default(), Default::default())),
+            Message::PullAll,
+        ];
+        client.pipeline(messages).await.unwrap();
+        let response = client.commit().await.unwrap();
+        assert!(Success::try_from(response).is_ok());
+
+        let messages = vec![
+            Message::RunWithMetadata(RunWithMetadata::new(
+                "MATCH (n {test: 'v3-commit'}) RETURN n;".to_string(),
+                Default::default(),
+                Default::default(),
+            )),
+            Message::PullAll,
+        ];
+        let mut node_exists = false;
+        for response in client.pipeline(messages).await.unwrap() {
+            if let Message::Record(record) = response {
+                let node =
+                    Node::try_from(Record::try_from(record).unwrap().fields()[0].clone()).unwrap();
+                assert_eq!(node.labels(), &["Database".to_string()]);
+                node_exists = true;
+                break;
+            }
+        }
+        assert!(node_exists);
+    }
+
+    #[tokio::test]
+    async fn commit_with_no_begin_fails() {
+        let client = get_initialized_client(3).await;
+        skip_if_handshake_failed!(client);
+        let mut client = client.unwrap();
+        let response = client.commit().await.unwrap();
+        assert!(Failure::try_from(response).is_ok());
+    }
+
+    #[tokio::test]
+    async fn rollback_empty_transaction() {
+        let client = get_initialized_client(3).await;
+        skip_if_handshake_failed!(client);
+        let mut client = client.unwrap();
+        let metadata = HashMap::<std::string::String, bool>::new(); // dummy empty metadata
+        client.begin(metadata).await.unwrap();
+        let response = client.rollback().await.unwrap();
+        assert!(Success::try_from(response).is_ok());
+    }
+
+    #[tokio::test]
+    async fn rollback() {
+        let client = get_initialized_client(3).await;
+        skip_if_handshake_failed!(client);
+        let mut client = client.unwrap();
+        let metadata = HashMap::<std::string::String, bool>::new(); // dummy empty metadata
+        client.begin(metadata).await.unwrap();
+        let messages = vec![
+            Message::RunWithMetadata(RunWithMetadata::new(
+                "MATCH (n {test: 'v3-rollback'}) DETACH DELETE n;".to_string(),
+                Default::default(), Default::default())),
+            Message::PullAll,
+            Message::RunWithMetadata(RunWithMetadata::new(
+                "CREATE (:Database {name: 'neo4j', v1_release: date('2010-02-16'), test: 'v3-rollback'});".to_string(),
+                Default::default(), Default::default())),
+            Message::PullAll,
+        ];
+        client.pipeline(messages).await.unwrap();
+        let response = client.rollback().await.unwrap();
+        assert!(Success::try_from(response).is_ok());
+
+        let messages = vec![
+            Message::RunWithMetadata(RunWithMetadata::new(
+                "MATCH (n {test: 'v3-rollback'}) RETURN n;".to_string(),
+                Default::default(),
+                Default::default(),
+            )),
+            Message::PullAll,
+        ];
+        for response in client.pipeline(messages).await.unwrap() {
+            assert!(match response {
+                Message::Success(_) => true,
+                // There should be no RECORD messages
+                _ => false,
+            });
+        }
+    }
+
+    #[tokio::test]
+    async fn rollback_with_no_begin_fails() {
+        let client = get_initialized_client(3).await;
+        skip_if_handshake_failed!(client);
+        let mut client = client.unwrap();
+        let response = client.rollback().await.unwrap();
+        assert!(Failure::try_from(response).is_ok());
     }
 }
