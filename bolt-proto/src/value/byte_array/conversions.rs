2c1f69aa2498ccad30718f8d8e77f138c96fbdfb
Luc Street <6709262+0xSiO@users.noreply.github.com>
2020-05-12 15:31:03 -0700

    Move 'TryInto<T> for Value' conversions to 'TryFrom<Value> for T'
    - Add missing Value conversions for &[u8] and Vec<u8> (for byte arrays)

--- bolt-proto/src/value/byte_array/conversions.rs
+++ bolt-proto/src/value/byte_array/conversions.rs
@@ -1,4 +1,8 @@
+use std::convert::TryFrom;
+
+use crate::error::*;
 use crate::value::ByteArray;
+use crate::Value;
 
 impl From<&[u8]> for ByteArray {
     fn from(value: &[u8]) -> Self {
@@ -14,11 +18,22 @@ impl From<Vec<u8>> for ByteArray {
     }
 }
 
+// We don't need TryFrom<Value> for ByteArray since it can be converted directly into a Vec
+// impl_try_from_value!(ByteArray, Bytes);
+
 impl From<ByteArray> for Vec<u8> {
     fn from(byte_array: ByteArray) -> Self {
         byte_array.value
     }
 }
 
-// We don't need TryFrom<Value> for ByteArray since it can be converted directly into a Vec
-// impl_try_from_value!(ByteArray, Bytes);
+impl TryFrom<Value> for Vec<u8> {
+    type Error = Error;
+
+    fn try_from(value: Value) -> Result<Self> {
+        match value {
+            Value::Bytes(byte_array) => Ok(byte_array.value),
+            _ => Err(ConversionError::FromValue(value).into()),
+        }
+    }
+}
