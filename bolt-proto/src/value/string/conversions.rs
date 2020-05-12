738ed23bf44156a59e8ffdde648987b9fa3495b4
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-05-12 14:46:57 -0700

    Change String try_into impl to try_from

--- bolt-proto/src/value/string/conversions.rs
+++ bolt-proto/src/value/string/conversions.rs
@@ -1,5 +1,8 @@
-use crate::impl_try_from_value;
+use std::convert::TryFrom;
+
+use crate::error::*;
 use crate::value::String;
+use crate::{impl_try_from_value, Value};
 
 impl From<&str> for String {
     fn from(value: &str) -> Self {
@@ -15,10 +18,21 @@ impl From<std::string::String> for String {
     }
 }
 
+impl_try_from_value!(String, String);
+
 impl From<String> for std::string::String {
     fn from(string: String) -> Self {
         string.value
     }
 }
 
-impl_try_from_value!(String, String);
+impl TryFrom<Value> for std::string::String {
+    type Error = Error;
+
+    fn try_from(value: Value) -> Result<Self> {
+        match value {
+            Value::String(string) => Ok(string.value),
+            _ => Err(ConversionError::FromValue(value).into()),
+        }
+    }
+}
