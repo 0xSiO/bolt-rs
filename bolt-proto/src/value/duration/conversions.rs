151e5846ecb99e0703fb7607c66c4a2a588b7d23
Luc Street <6709262+0xSiO@users.noreply.github.com>
2020-05-16 00:23:49 -0700

    Remove references to deprecated max_value function

--- bolt-proto/src/value/duration/conversions.rs
+++ bolt-proto/src/value/duration/conversions.rs
@@ -5,11 +5,11 @@ use crate::value::Duration;
 
 impl From<StdDuration> for Duration {
     fn from(duration: StdDuration) -> Self {
-        // This fits in an i64 because u64::max_value() / (3600 * 24) < i64::max_value()
+        // This fits in an i64 because u64::MAX / (3600 * 24) < i64::MAX
         let days = (duration.as_secs() / (3600 * 24)) as i64;
         // This fits in an i64 since it will be less than 3600 * 24
         let seconds = (duration.as_secs() % (3600 * 24)) as i64;
-        // This fits in an i32 because 0 <= nanos < 1e9 which is less than i32::max_value()
+        // This fits in an i32 because 0 <= nanos < 1e9 which is less than i32::MAX
         let nanos = duration.subsec_nanos() as i32;
 
         // Months are not well-defined in terms of seconds so let's not use them here
