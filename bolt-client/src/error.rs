3603f135e6371a7c8f8895061f8d9c807938fbb7
Luc Street <6709262+0xSiO@users.noreply.github.com>
2021-07-09 22:44:15 -0700

    Remove unused error variants from bolt-client

--- bolt-client/src/error.rs
+++ bolt-client/src/error.rs
@@ -5,36 +5,14 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub type ConnectionResult<T> = std::result::Result<T, ConnectionError>;
 pub type CommunicationResult<T> = std::result::Result<T, CommunicationError>;
 
-// TODO: Break into more specific error types
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error(transparent)]
-    IOError(#[from] std::io::Error),
-    #[error("handshake with server failed for versions [{}]", format_versions(.0))]
-    HandshakeFailed([u32; 4]),
-    #[error("unsupported operation for client with version = {}", format_version(*.0))]
-    UnsupportedOperation(u32),
-    #[error("unsupported operation for server in {state:?} state: {message:?}")]
-    InvalidState {
-        state: bolt_proto::ServerState,
-        message: bolt_proto::Message,
-    },
-    #[error(
-        "server gave unexpected response while in {state:?} state.
-request: {request:?}
-response: {response:?}"
-    )]
-    InvalidResponse {
-        state: bolt_proto::ServerState,
-        request: Option<bolt_proto::Message>,
-        response: bolt_proto::Message,
-    },
     #[error(transparent)]
     ConnectionError(#[from] ConnectionError),
     #[error(transparent)]
     CommunicationError(#[from] CommunicationError),
     #[error(transparent)]
-    ProtocolError(#[from] bolt_proto::error::Error),
+    ProtocolError(#[from] ProtocolError),
 }
 
 #[derive(Debug, Error)]
